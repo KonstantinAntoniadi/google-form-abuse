db0c8c11b25303c1659a5bed721d76ea50f308ea
Edwin Evia <edwinevi@student.21-school.ru>
2023-09-07 03:12:13 +0300

    create def: create_non_empty_list

--- abuse.py
+++ abuse.py
@@ -2,11 +2,17 @@ from storage import *
 from settings import *
 import time
 
+# Create a list with keys whose value is not zero
+def create_non_empty_list(count):
+    return [key for key, value in count.items() if value != 0]
+
 def abuse(driver, url):
     try:
         driver.get(URL)
         map_storage = MapStorage(MAP_PATH)
         count = map_storage.Read()
+        valid_keys = create_non_empty_list(count)
+        print(valid_keys)
         time.sleep(5)
     except Exception as ex:
         print(ex)
