2ad0c9ef8ff8f14e7004a179ad4e4c9e36432938
Edwin Evia <edwinevi@student.21-school.ru>
2023-09-07 03:14:58 +0300

    create def get_random_key

--- abuse.py
+++ abuse.py
@@ -1,10 +1,16 @@
 from storage import *
 from settings import *
 import time
+import random
 
 # Create a list with keys whose value is not zero
 def create_non_empty_list(count):
     return [key for key, value in count.items() if value != 0]
+
+def get_random_key(keys):
+    random_key = random.choice(keys)
+    # TODO: add logic for reducing the value
+    return random_key
 
 def abuse(driver, url):
     try:
@@ -12,7 +18,8 @@ def abuse(driver, url):
         map_storage = MapStorage(MAP_PATH)
         count = map_storage.Read()
         valid_keys = create_non_empty_list(count)
-        print(valid_keys)
+        random_key = get_random_key(valid_keys)
+        print(random_key)
         time.sleep(5)
     except Exception as ex:
         print(ex)
