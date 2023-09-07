2987648bc7823ac848a30648b0e0720917bdabd1
Edwin Evia <edwinevi@student.21-school.ru>
2023-09-07 03:04:45 +0300

    crete pickle converter

---
+++ pickle_converter.py
@@ -0,0 +1,20 @@
+import pickle
+from settings import MAP_PATH
+from data import count
+
+def write():
+    with open(MAP_PATH, 'wb') as file:
+        pickle.dump(count, file)
+
+def read():
+    try:
+        with open(MAP_PATH, 'rb') as file:
+            count = pickle.load(file)
+    except FileNotFoundError:
+        count = {}
+    
+    for key, value in count.items():
+        print(f'{key}: {value}')
+
+# write()
+# read()
