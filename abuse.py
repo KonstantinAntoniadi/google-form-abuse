42684082fce3562f2fe718a804bb592c52a1ac62
Edwin Evia <edwinevi@student.21-school.ru>
2023-09-07 03:08:28 +0300

    decompose

--- abuse.py
+++ abuse.py
@@ -1,2 +1,16 @@
-def abuse(url):
+from storage import *
+from settings import *
+import time
+
+def abuse(driver, url):
+    try:
+        driver.get(URL)
+        map_storage = MapStorage(MAP_PATH)
+        count = map_storage.Read()
+        time.sleep(5)
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
     
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from driver import *
 from settings import * 
-import time
+from abuse import *
 
 if __name__ == '__main__':
     driver = set_up_driver("./chromedriver/chromedriver")
-    try:
-        driver.get(URL)
-        time.sleep(5)
-    except Exception as ex:
-        print(ex)
-    finally:
-        driver.close()
-        driver.quit()
+    abuse(driver, URL)
